69b0c82cdfcee4dd94d0c30e360a20b49b2a743e
JudSze <77547600+JudSze@users.noreply.github.com>
2023-04-17 14:35:17 +0200

    Update preproc_remove_dups.py

--- mouse/preproc_remove_dups.py
+++ mouse/preproc_remove_dups.py
@@ -1,17 +1,17 @@
 import pandas as pd
 
 # Columns in this order: chromosome, sample ID, position, reference genome, mutation, 
-d10_normal = pd.read_csv("input_data.csv", sep = ";") 
+coselens_input = pd.read_csv("input_data.csv", sep = ";") 
 
 #remove last column vaf
-d10_normal.head()
-d10_normal.info # note the dimension of the dataset at the beginning
+coselens_input.head()
+coselens_input.info # note the dimension of the dataset at the beginning
 
 # Super safe dataset
 # Remove samples with mutations, that are in the same mouse, and are suspiciously similar
 # It can also be because of policlonality
-cleared_d10_normal = d10_normal.drop_duplicates(subset= ["mouse","pos","ref","mut","gene","ntchange","aachange","impact"],keep="first")
-cleared_d10_normal.info # note the dimension of the cleared dataset ó
+coselens_input = coselens_input.drop_duplicates(subset= ["mouse","pos","ref","mut","gene","ntchange","aachange","impact"],keep="first")
+coselens_input.info # note the dimension of the cleared dataset ó
 
 
-cleared_d10_normal.to_csv("cleared_input_data.csv", sep = ";")
+coselens_input.to_csv("cleared_input_data.csv", sep = ";")
