ba14ba5131029c89d4e357039cf62a26c9a9ec46
JudSze <77547600+JudSze@users.noreply.github.com>
2023-04-17 14:58:48 +0200

    Create covs_matrice_mice.R

---
+++ mouse/covariates/covs_matrice_mice.R
@@ -0,0 +1,54 @@
+library(readxl)
+library(factoextra)
+
+expression = read.csv("expression_table.csv", sep = ";", header = TRUE)
+row.names(expression) = expression$Gene_name
+expression$Gene_name = NULL
+t_expression = t(expression)
+
+#PCA with prcomp
+pca = prcomp(res.comp$completeObs)
+fviz_eig(res.pca, addlabels = TRUE, ncp = 24)
+fviz_eig(res.pca, geom = "line")
+
+pca_matrice = as.data.frame(pca$x)
+write.csv(pca_matrice, "pca_matrice.csv")
+
+getwd()
+setwd("your_path")
+saveRDS(pca, "mean_expression_pca.rds")
+head(pca, 2)
+
+fviz_eig(pca, addlabels = TRUE, ncp = 15)
+
+# PCA with missing values
+# install.packages("missMDA")
+# install.packages("FactoMineR")
+
+library(missMDA)
+library(FactoMineR)
+
+# estimate the number of dimensions for PCA
+# ncp.max is the maximum number of components to test, I would recommend using the number of columns as the value for this parameter
+nb = estim_ncpPCA(expression, method.cv = "Kfold", ncp.min = 0, ncp.max = 15, verbose = FALSE)
+nb$ncp # note the recommended dimension
+
+plot(0:5, nb$criterion, xlab = "nb dim", ylab = "MSEP")
+
+# compute PCA
+res.comp <- imputePCA(expression)
+View(res.comp$completeObs)
+
+res.pca <- PCA(res.comp$completeObs, ncp = 15 ,graph=TRUE)
+
+head(res.pca$ind$coord) #scores (principal components)
+head(res.pca$svd)
+
+setwd("your_path")
+write.csv(res.pca$ind$coord, "cov_table.csv")
+write.csv(res.pca$ind$contrib, "contrib_table.csv")
+
+fviz_contrib(res.pca, choice = "var", axes = 5, top = 10)
+
+fviz_eig(res.pca, addlabels = TRUE, ncp = 24)
+fviz_eig(res.pca, geom = "line")
